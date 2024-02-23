ea5f4d413f490a47fe3057a0a5cb28c8d6473f56
Mattsll99 <matthiassylla21@gmail.com>
2024-02-23 19:13:36 +0100

    gowup

--- src/app/entrée/page.js
+++ src/app/entrée/page.js
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import Button from '@/components/Button'
 import Section1 from '@/components/Section1'
